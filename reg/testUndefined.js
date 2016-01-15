e76018b4ef77f58b6f1bfda5cfcb7a7ad7ab6c62
WangLiang <wangliang@WangLiangs.local>
2016-01-15 10:02:02 +0800

    track reg/testUndefined.js

---
+++ reg/testUndefined.js
@@ -0,0 +1,9 @@
+ this.name=undefined;
+ if(this.name) {
+     console.log("name:"+this.name);
+ }
+     console.log("name:"+this.name);
+ this.name="I am leo";
+ if(this.name) {
+     console.log("name:"+this.name);
+ }
