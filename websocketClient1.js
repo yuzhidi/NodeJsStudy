b70c555fcf6a623c46a9321ef603b07fdb946ac8
wangliang <wangliang@testbird.com>
2016-02-21 17:49:55 +0800

    websocket client can get certain frame

--- websocketClient1.js
+++ websocketClient1.js
@@ -7,10 +7,20 @@ var ws = new WebSocket('ws://127.0.0.1:8080');
 ws.on('open', function open() {
     ws.send('something from client');
 });
-
+var count = 0;
+var get = false;
+var TOTAL = 3;
 ws.on('message', function(data, flags) {
     // flags.binary will be set if a binary data is received.
     // flags.masked will be set if the data was masked.
     //console.log(data);
     process.stdout.write(data);
+    if(get) {
+        if(++count == TOTAL) {
+            ws.close();
+        }
+    }
+    else {
+        ws.close();
+    }
 });
