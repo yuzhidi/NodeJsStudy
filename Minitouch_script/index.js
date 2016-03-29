00979bf2fe4090a5a0164f481f3b9eae02855b62
Leocone <wlia@163.com>
2016-03-29 18:16:29 +0800

    save code to file

--- Minitouch_script/index.js
+++ Minitouch_script/index.js
@@ -2,18 +2,38 @@
  * Created by wangliang on 29/3/2016.
  */
 var fs = require("fs");
+var util = require('util');
 //var data = fs.readFileSync("minitouch.script");
-var array;
 //console.log(data.toString());
+//var codeTemplete = "setTimeout(function() {console.log("
+
+function codeFunction(string) {
+    return util.format("setTimeout(function() {console.log(%s)}, %s);",string, string);
+}
+
 fs.readFile('minitouch.script', function(err, data) {
     if(err) throw err;
     //array = data.toString().split("\n");
-    array = data.toString().split("\n");
-    var outfile = fs.createWriteStream('array.txt');
+    var array = data.toString().split("\n");
+    var outfile = fs.createWriteStream('out.script');
+    var basetime;
+    var currenttime;
     outfile.on('error', function(err) { /* error handling */ });
     for(i in array) {
-        console.log(array[i]);
-        outfile.write(array[i]+"\n");
+        //console.log(array[i]);
+        //outfile.write(array[i]+"\n");
+        if(i == 0) {
+            basetime = array[i].split(" ")[0];
+            console.log("basetime", basetime);
+        }
+        currenttime = array[i].split(" ")[0];
+        var time_out = currenttime - basetime;
+        if(time_out < 0) {
+            continue;
+        }
+        //console.log(codeFunction(time_out));
+        outfile.write(codeFunction(time_out)+"\n");
+
     }
     outfile.end();
 });
